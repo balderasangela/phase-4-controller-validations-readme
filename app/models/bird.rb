45c734a6157b73ff02ada1b4b27c0243a9fe7b17
Angela Balderas <balderasangela@gmail.com>
2023-03-11 13:47:05 -0500

    completed assignment

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,16 @@
 class Bird < ApplicationRecord
+   bird = Bird.create!(bird_params)
+  render json: bird, status: :created
+rescue ActiveRecord::RecordInvalid => invalid
+  render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+
+
+  def update
+    bird = find_bird
+    bird.update!(bird_params)
+    render json: bird
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+  
 end
